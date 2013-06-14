0566a85e4f9935c86139d3ebcb0b4472af9f3535
fzannettacci <faustine.zannettacci@elao.com>
2013-06-14 17:18:24 +0200

    Inte

---
+++ web/config.rb
@@ -0,0 +1,31 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+
+# Production Assets URL
+http_images_path = "/img/"
+relative_assets = false
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+#line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
