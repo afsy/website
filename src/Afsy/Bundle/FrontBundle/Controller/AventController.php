689d96ef77ef122e88b9a4cdf5efc98fad509659
Hugo Hamon <hugo.hamon@sensiolabs.com>
2013-12-01 21:10:58 +0100

    [AfsyFrontBundle] added day 2 article for advent calendar

--- src/Afsy/Bundle/FrontBundle/Controller/AventController.php
+++ src/Afsy/Bundle/FrontBundle/Controller/AventController.php
@@ -14,15 +14,8 @@ class AventController extends Controller
     protected $slugs = array(
         // Last articles top
         2013 => array(
-            '02-code-stupid-solid' => "AfsyFrontBundle:Avent:day_2013_02.html.twig",
-            '01-presentation-afsy' => "AfsyFrontBundle:Avent:day_2013_01.html.twig"
-
-
-            /*'05-Komen-Kon-Fé-Du-PHP' => "AfsyFrontBundle:Avent:day_2013_01.html.twig",
-            '04-Komen-Kon-Fé-Du-PHP' => "AfsyFrontBundle:Avent:day_2013_01.html.twig",
-            '03-Komen-Kon-Fé-Du-PHP' => "AfsyFrontBundle:Avent:day_2013_01.html.twig",
-            '02-Komen-Kon-Fé-Du-PHP' => "AfsyFrontBundle:Avent:day_2013_01.html.twig",
-            '01-Komen-Kon-Fé-Du-PHP' => "AfsyFrontBundle:Avent:day_2013_01.html.twig",*/
+            '02-principes-stupid-solid-poo' => "AfsyFrontBundle:Avent:day_2013_02.html.twig",
+            '01-presentation-afsy'          => "AfsyFrontBundle:Avent:day_2013_01.html.twig",
         )
     );
 
