f588166c9b6e00f40a0636983f5554d57097fa18
Nek (Maxime Veber) <nek.dev@gmail.com>
2019-11-30 18:37:38 +0100

    Configure properly prismjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,30 @@ import Prism from 'prismjs';
 
 // Import PrismJS extensions
 import 'prismjs/themes/prism-twilight.css';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-css-extras';
 import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-php-extras';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-javadoclike';
+import 'prismjs/components/prism-javadoc';
+import 'prismjs/components/prism-phpdoc';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-yaml';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsonp';
+import 'prismjs/components/prism-json5';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-twig';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-graphql';
+import 'prismjs/components/prism-diff';
+// Note: this is huge because their autoloader does not work with webpack...
 
 // Import Prism JS
 import 'prismjs/plugins/line-numbers/prism-line-numbers';
