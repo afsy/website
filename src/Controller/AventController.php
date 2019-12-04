fb855963e197c44e7e21116aa52ae0c59db8bdad
nayluge <cyril.souillard@gmail.com>
2019-12-04 16:02:27 +0100

    article 5 : structurez votre monolithe

--- src/Controller/AventController.php
+++ src/Controller/AventController.php
@@ -79,6 +79,7 @@ class AventController extends AbstractController
             '01-easyadminbundle-l-arriere-guichet-easy-peasy' => 'Avent/2017/day_01.html.twig',
         ),
         2019 => array(
+            '05-structurez-votre-monolithe' => 'Avent/2019/day_05.html.twig',
             '04-ne-me-parlez-plus-de-manager' => 'Avent/2019/day_04.html.twig',
             '03-symfony-et-test-technique' => 'Avent/2019/day_03.html.twig',
             '02-symfony4-vers-symfony5' => 'Avent/2019/day_02.html.twig',
